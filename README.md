718757cd0c5d2c3d6023a925ec3342206ddc2f86
Matthew Adams <matthew.adams@hpe.com>
2018-03-19 12:36:51 +0000

    Basic build working

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # hellopython
+
+Simple hellow world in python that replies over html
+
+Build docker image 
+docker build -t domesticgod/hellopython .
+
+Run
+docker run -it -P --rm domesticgod/hellopython
---
+++ helloworld.py
@@ -0,0 +1,19 @@
+import http.server
+import socketserver
+
+PORT = 8080
+
+class MyHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"<html><head><title>Title goes here.</title></head>")
+        self.wfile.write(b"<body><p>Hello World!</p></body></html>")
+try:
+    server = http.server.HTTPServer(('', PORT), MyHandler)
+    print('Started http server')
+    server.serve_forever()
+except KeyboardInterrupt:
+    print('^C received, shutting down server')
+    server.socket.close()
